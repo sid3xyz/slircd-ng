32682a542ea136cc64f597db854d504ef93154de
Sidney M Field III <sid@sid3.xyz>
2025-12-02 07:11:21 -0500

    feat: add MAP, RULES, USERIP, LINKS handlers
    Server query commands:
    - MAP: Display server map (single-server topology)
    - RULES: Display server rules (RPL_RULESTART/RPL_RULES/RPL_ENDOFRULES)
    - USERIP: Get IP addresses of users (oper-only, returns RPL_USERIP)
    - LINKS: Show server links (single-server response)

--- src/handlers/server_query/mod.rs
+++ src/handlers/server_query/mod.rs
@@ -1,9 +1,11 @@
-//! Server query handlers: VERSION, TIME, ADMIN, INFO, LUSERS, STATS, MOTD
+//! Server query handlers: VERSION, TIME, ADMIN, INFO, LUSERS, STATS, MOTD, MAP, RULES, USERIP, LINKS
 //!
 //! RFC 2812 §3.4 - Server queries and commands
 
+mod info;
 mod motd;
 mod stats;
 
+pub use info::{LinksHandler, MapHandler, RulesHandler, UseripHandler};
 pub use motd::{AdminHandler, InfoHandler, ListHandler, LusersHandler, MotdHandler, TimeHandler, VersionHandler};
 pub use stats::StatsHandler;
