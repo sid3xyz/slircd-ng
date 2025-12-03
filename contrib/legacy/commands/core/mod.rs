f57423534ac9b84c2115ed76dfd22a5efe657c3b
Sidney M Field III <sid@sid3.xyz>
2025-12-03 10:30:21 -0500

    chore: move contrib/legacy to workspace root

--- contrib/legacy/commands/core/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Core IRC commands module
-
-pub mod nick;
-pub mod user;
-pub mod mode;
-pub mod privmsg;
-pub mod ping;
-pub mod quit;
--- contrib/legacy/infrastructure/mod.rs
+++
@@ -1,6 +0,0 @@
-// Infrastructure layer
-// Shared services: config, persistence, observability
-
-pub mod config;
-pub mod persistence;
-pub mod observability;
