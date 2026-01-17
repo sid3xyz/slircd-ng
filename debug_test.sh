d15ca39ed51d08632184c7bf51bde01e5094526b
Sidney M Field III <sid@sid3.xyz>
2026-01-16 20:40:30 -0500

    Remove debug scripts

--- debug_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export RUST_LOG=debug
-./scripts/irctest_safe.sh slirc-irctest/irctest/server_tests/test_bouncer_multiclient.py::MulticlientTestCase::testQuit > irctest_debug_output.txt 2>&1
